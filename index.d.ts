64596a8c63a074af47fe84d312940eae5e899cd9
IvanKhartov <ivan.khartov@gmail.com>
2024-01-05 13:39:18 +0200

    Refine type declarations in TypeScript

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,8 @@
 import { Directive, MaskElement, Plugin, PluginOptions } from './src/types'
 
-declare const Vue3Mask: Plugin<PluginOptions & undefined>
-declare const Vue3MaskPlugin: Plugin<PluginOptions & undefined>
+declare const Vue3Mask: Plugin<PluginOptions>
+declare const Vue3MaskPlugin: Plugin<PluginOptions>
 declare const Vue3MaskDirective: Directive<MaskElement>
 
-export { Vue3Mask as default, Vue3MaskPlugin, Vue3MaskDirective }
+export { Vue3MaskPlugin, Vue3MaskDirective }
+export default Vue3Mask
